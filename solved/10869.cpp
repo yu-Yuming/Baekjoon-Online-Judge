8a67d7ab310e0920833f36615390eca18d9d2074
yu-Yuming <78905337+yu-Yuming@users.noreply.github.com>
2025-01-02 12:13:55 +0900

    10869

---
+++ solved/10869.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int A, B;
+    std::cin >> A >> B;
+
+    std::cout << A + B << std::endl;
+    std::cout << A - B << std::endl;
+    std::cout << A * B << std::endl;
+    std::cout << A / B << std::endl;
+    std::cout << A % B << std::endl;
+
+    return 0;
+}
