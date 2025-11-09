7c7caf77335f0ba50fbc84e8b17d0fdf67fe9081
yu-Yuming <78905337+yu-Yuming@users.noreply.github.com>
2025-11-09 17:33:27 +0900

    14581

---
+++ solved/14581.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string id;
+    std::cin >> id;
+
+    std::cout << ":fan::fan::fan:" << std::endl;
+    std::cout << ":fan::" << id << "::fan:" << std::endl;
+    std::cout << ":fan::fan::fan:" << std::endl;
+
+    return 0;
+}
